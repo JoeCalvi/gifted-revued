237f358a55f0a0d448fc13aca6eb2cca2d7b3649
JoeCalvi <joecalvi@me.com>
2023-02-21 15:58:06 -0700

    giftcard template drawing and gtg

---
+++ src/components/GiftCard.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="giftcard">
+        <div class="card border-dark">
+            <div v-if="gift.opened == false">
+                <img src="https://www.pngitem.com/pimgs/m/215-2152100_mystery-box-png-transparent-background-gift-box-png.png" 
+                class="gift rounded mb-2" alt="">
+            </div>
+            <div v-else="gift.opened == true">
+                <img :src="gift.url" class="gift rounded mb-2" alt="">
+            </div>
+            <div class="text-center">
+                <p>{{ gift.tag }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import { Gift } from '../models/Gift.js'
+
+export default {
+
+    props: {
+        gift: { type: Gift}
+    },
+
+    setup(){
+        return {}
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+.gift{
+  height: 250px;
+  width: 250px;
+}
+
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,14 +1,9 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img
-        src="https://bcw.blob.core.windows.net/public/img/8600856373152463"
-        alt="CodeWorks Logo"
-        class="rounded-circle"
-      >
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
+  <div class="container-fluid">
+    <div class="row">
+      <div v-for="gift in gifts" class="col-md-4 my-3">
+        <GiftCard :gift="gift"/>
+      </div>
     </div>
   </div>
 </template>
@@ -17,7 +12,8 @@
 import { logger } from '../utils/Logger.js';
 import Pop from '../utils/Pop.js';
 import { giftsService } from '../services/GiftsService.js'
-import { onMounted } from 'vue';
+import { onMounted, computed } from 'vue';
+import { AppState } from '../AppState.js';
 
 export default {
   setup() {
@@ -30,34 +26,25 @@ export default {
         logger.log('[getGifts error]', error)
       }
     }
+
     onMounted(() => {
       getGifts()
     })
-    
-    return {}
-    // NOTE PUBLIC
+
+    return {
+      // NOTE PUBLIC
+      gifts: computed(() => AppState.gifts)
+    }
+  
   }
 }
 </script>
 
 <style scoped lang="scss">
-.home {
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
-
-  .home-card {
-    width: 50vw;
-
-    >img {
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
+.gift{
+  height: 250px;
+  width: 250px;
 }
+
+
 </style>
